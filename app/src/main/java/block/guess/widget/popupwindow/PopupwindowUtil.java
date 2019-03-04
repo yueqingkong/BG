c70cadbbc45b85448de707762c3be62694bebd97
puin <762396990@qq.com>
2019-03-04 11:08:04 +0800

    优化 弹框背景透明度

--- app/src/main/java/block/guess/widget/popupwindow/PopupwindowUtil.java
+++ app/src/main/java/block/guess/widget/popupwindow/PopupwindowUtil.java
@@ -1,18 +1,24 @@
 package block.guess.widget.popupwindow;
 
 import android.app.Activity;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.View;
 
+import android.view.WindowManager;
+import android.widget.PopupWindow;
 import block.guess.utils.DensityUtils;
 import block.guess.widget.popupwindow.bean.PopupCallback;
 import block.guess.widget.popupwindow.common.BasePopupwindow;
 
 public class PopupwindowUtil {
 
-    public static void show(Activity activity, int resid, final View bottomView, int xOff, final int yOff, PopupCallback callback) {
+    public static void show(final Activity activity, int resid, final View bottomView, int xOff, final int yOff, PopupCallback callback) {
         final BasePopupwindow basePopupwindow = new BasePopupwindow(activity, resid);
         View baseView = basePopupwindow.getBaseView();
         basePopupwindow.showAsDropDown(bottomView, xOff, yOff);
+        basePopupwindow.setBackgroundDrawable(new BitmapDrawable());
+        backgroundAlpha(activity, 0.8f);
+
         basePopupwindow.getContentView().post(new Runnable() {
             @Override
             public void run() {
@@ -22,7 +28,25 @@ public class PopupwindowUtil {
                 basePopupwindow.update(bottomView, offsetX, yOff, width, height);
             }
         });
+        basePopupwindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                backgroundAlpha(activity, 1f);
+            }
+        });
 
         callback.createView(basePopupwindow, baseView);
+    }
+
+    /**
+     * 设置背景透明度
+     *
+     * @param activity
+     * @param alpha    0.0-1.0
+     */
+    private static void backgroundAlpha(Activity activity, float alpha) {
+        WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+        lp.alpha = alpha;
+        activity.getWindow().setAttributes(lp);
     }
 }
